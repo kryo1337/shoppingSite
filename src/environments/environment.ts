cc0d56857c86e6e97083ab57abee2594fb7efb39
kryo1337 <kryo1337@gmail.com>
2023-05-19 18:46:08 +0100

    dodanie html css i firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'shoppingsiteveas',
+    appId: '1:344912538046:web:00f07073dd8d7a7d2931ef',
+    storageBucket: 'shoppingsiteveas.appspot.com',
+    locationId: 'europe-west2',
+    apiKey: 'AIzaSyA90kn7oeYKwIQPlM9DjTZDGtosGxW4nJY',
+    authDomain: 'shoppingsiteveas.firebaseapp.com',
+    messagingSenderId: '344912538046',
+    measurementId: 'G-9TQY27X2SR',
+  }
+};
